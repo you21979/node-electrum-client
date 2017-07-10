0ac31651001ca21d0d3fb391909548adc7fb4c27
you21979 <you2197901@gmail.com>
2017-07-10 20:25:34 +0900

    mod

--- example/simple.js
+++ example/simple.js
@@ -6,6 +6,10 @@ const main = async () => {
     try{
         const ver = await ecl.server_version("2.7.11", "1.0")
         console.log(ver)
+        const balance = await ecl.blockchainAddress_getBalance("12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX")
+        console.log(balance)
+        const unspent = await ecl.blockchainAddress_listunspent("12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX")
+        console.log(unspent)
     }catch(e){
         console.log(e)
     }
