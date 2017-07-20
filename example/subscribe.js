50e43d288e67f2648155123adb8a4757edfda813
you21979 <you2197901@gmail.com>
2017-07-20 14:05:16 +0900

    connect error handling

--- example/subscribe.js
+++ example/subscribe.js
@@ -2,17 +2,23 @@ const ElectrumClient = require('..')
 const sleep = (ms) => new Promise((resolve,_) => setTimeout(() => resolve(), ms))
 
 const main = async () => {
-    const ecl = new ElectrumClient(995, 'btc.smsys.me', 'tls')
-    ecl.subscribe.on('server.peers.subscribe', console.log)
-    ecl.subscribe.on('blockchain.numblocks.subscribe', console.log)
-    ecl.subscribe.on('blockchain.headers.subscribe', console.log)
-    await ecl.connect()
-    const p1 = await ecl.serverPeers_subscribe()
-    const p2 = await ecl.blockchainHeaders_subscribe()
-    const p3 = await ecl.blockchainNumblocks_subscribe()
-    while(true){
-        await sleep(1000)
+    try{
+        const ecl = new ElectrumClient(995, 'btc.smsys.me', 'tls')
+        ecl.subscribe.on('server.peers.subscribe', console.log)
+        ecl.subscribe.on('blockchain.numblocks.subscribe', console.log)
+        ecl.subscribe.on('blockchain.headers.subscribe', console.log)
+        await ecl.connect()
+        const p1 = await ecl.serverPeers_subscribe()
+        const p2 = await ecl.blockchainHeaders_subscribe()
+        const p3 = await ecl.blockchainNumblocks_subscribe()
+        while(true){
+            await sleep(1000)
+            let version = await ecl.server_version("2.7.11", "1.0")
+        }
+        await ecl.close()
+    }catch(e){
+        console.log("error")
+        console.log(e)
     }
-    await ecl.close()
 }
 main()
