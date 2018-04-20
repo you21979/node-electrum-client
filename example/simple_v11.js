7ace4fa447c9ec746de121663c0f394cf27a3dd7
you21979 <you2197901@gmail.com>
2018-04-20 16:07:25 +0900

    add other version

---
+++ example/simple_v11.js
@@ -0,0 +1,45 @@
+const electrumclient = require('..')
+const Client = electrumclient.Client
+const ElectrumProtocol = electrumclient.v3.ElectrumProtocol
+
+const proc = async (ecl) => {
+    const balance = await ecl.blockchain_address_getBalance("12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX")
+    console.log(balance)
+    const unspent = await ecl.blockchain_address_listunspent("12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX")
+    console.log(unspent)
+
+    const tx1 = await ecl.blockchain_transaction_get("f91d0a8a78462bc59398f2c5d7a84fcff491c26ba54c4833478b202796c8aafd")
+    console.log(tx1)
+}
+
+const main = async () => {
+    // autogenerate client name
+    const myname = [ElectrumProtocol.libname, ElectrumProtocol.hash].join('-')
+    console.log(myname)
+
+    // initialize
+    const ecl = new ElectrumProtocol(new Client(995, 'btc.smsys.me', 'tls'))
+
+    // wait a connection
+    await ecl.client.connect()
+
+    try{
+        // negotiation protocol
+        const res = await ecl.server_version(myname)
+        console.log(res)
+    }catch(e){
+        // negotiation error
+        await ecl.client.close()
+        console.log(e)
+        return;
+    }
+
+    try{
+        await proc(ecl)
+    }catch(e){
+        console.log(e)
+    }
+    await ecl.client.close()
+}
+main().catch(console.log)
+
